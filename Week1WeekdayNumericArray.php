4740ba5a4078335f2a32ab0685e5b14bc64becc8
foxh00 <00henryfox@gmail.com>
2024-11-23 18:47:00 +0000

    Add files via upload

---
+++ Week1WeekdayNumericArray.php
@@ -0,0 +1,9 @@
+<?php
+  echo"<h1>Numeric array with days of the week</h1>";
+// This will be a numeric array which will print the days of the week
+
+$weekdays = array( 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday', 'Friday', 'Saturday', 'Sunday' ) ;
+foreach( $weekdays as $value ) 
+{ echo " $value " ; } 
+?>
+
